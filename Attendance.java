2d36d5ee0e3fdcb3c3051d3b0c64efb4604928be
PRINCE-BODA <164193414+PRINCE-BODA@users.noreply.github.com>
2024-04-09 15:07:09 +0530

    Add files via upload

---
+++ Attendance.java
@@ -0,0 +1,8 @@
+
+package faculty;
+class Attendance{
+	public static void main(String[] args) {
+		System.out.println("Faculty Attendance");
+		System.out.println("name of the student is = "+student.Attendance.name);
+	}
+}
