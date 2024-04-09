a9f63a6bc983818bdc628800b923643d456ac4c1
PRINCE-BODA <164193414+PRINCE-BODA@users.noreply.github.com>
2024-04-09 15:14:17 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
-/* Problem 1 and 2 are theortical 
- *  This is Problem 3
- */
-public class HelloWorld{ 
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello world from Java");
+	}
 }
